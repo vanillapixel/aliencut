6a2d4323f7fcb79f2659d0142205fa29b7f468e2
vanillapixel <giorgio.adamo.ap@gmail.com>
2019-04-29 14:07:15 +0200

    sliderPod changes coloumn

--- assets/js/alien.js
+++ assets/js/alien.js
@@ -4,8 +4,7 @@ const intro = $(".intro"),
   audioButton = $(".speaker-icon-audio-on"),
   enterWebsiteButton = $(".enter-website"),
   audioStatus = $(".speaker-icon-audio-on:after"),
-  sidebar = $(".sidebar-container"),
-  sliderPod = document.querySelector('.slider-pod');
+  sidebar = $(".sidebar-container");
 
 $(video).prop("volume", 0.3);
 
@@ -54,7 +53,7 @@ eventsSlider = (_this, n) => {
   if ($(_this).hasClass("next")) {
     if (multiplier < n - 1) {
       multiplier = multiplier + 1;
-      sliderPod.style.transform = `translate(calc(100%*${multiplier} + 3*${multiplier}%)`
+      sliderPod.style.transform = `translate(calc(100%*${multiplier} + 3*${multiplier}%))`
       $(".event").css("transform", `translate(-${baseMovement * multiplier}%)`);
       if (multiplier === n - 1) $(".next").css("opacity", "0");
     }
@@ -65,7 +64,7 @@ eventsSlider = (_this, n) => {
   // if the controller button is 'previous'
   else if (multiplier > 0) {
     multiplier = multiplier - 1;
-    sliderPod.style.transform = `translate(calc(100%*${multiplier} + 3*${multiplier}%)`
+    sliderPod.style.transform = `translate(calc(100%*${multiplier} + 3*${multiplier}%))`
     $(".event").css("transform", `translate(-${baseMovement * multiplier}%)`);
     if (multiplier < n - 1) $(".next").css("opacity", "1");
     if (multiplier === 0) {
@@ -73,23 +72,35 @@ eventsSlider = (_this, n) => {
     }
   }
 };
+sliderPodHandler = (n) => {
+  const sliderPod = document.querySelector('.slider-pod')
+  // sets the slider pod width equally wide to the slider slots width
+  sliderPod.style.width = `calc(${100 / n}% - 2px)`
+  $(".slider-slot").click(function () {
+    let multiplier = 0;
+    multiplier = $('.slider-slot').index(this)
+    sliderPod.style.transform = `translate(calc(100%*${multiplier} + 3*${multiplier}%)`
+    $(".event").css("transform", `translate(-${baseMovement * multiplier}%)`);
+  });
+}
 createSliderSlots = (n) => {
   const sliderBar = document.querySelector('.slider-bar')
   for (let i = 0; i < n; i++) {
-    console.log(i)
     // creates a slider slot in the slider bar for each slider coloumn
     sliderBar.innerHTML += `<div class='slider-slot'></div>`
+    sliderPodHandler(n)
   }
-  const sliderPod = document.querySelector('.slider-pod')
-  // sets the slider pod width according to how many the slider slots are
-  sliderPod.style.width = `calc(${100 / n}% - 2px)`
 }
+
+// function calls
 
 $(".controller").click(function () {
   const eventsColoumns = Math.ceil($(".event").length / 3);
   eventsSlider(this, eventsColoumns);
 });
+
 document.addEventListener("DOMContentLoaded", () => {
   const eventsColoumns = Math.ceil($(".event").length / 3);
   createSliderSlots(eventsColoumns);
 })
+
