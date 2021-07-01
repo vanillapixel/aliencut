b5a0979f179d816dbf159e6831dc94f3c258242a
vanillapixel <giorgio.adamo.ap@gmail.com>
2021-07-01 12:33:26 +0200

    alientrax list update (june)

--- assets/js/alienTracksData.js
+++ assets/js/alienTracksData.js
@@ -1,272 +1,303 @@
 const remixesData = {
-  october20: {
-    id: "oct20-1",
-    date: {
-      month: "ottobre",
-      year: 2020,
-    },
-    songs: [
-      {
-        id: "oct20-g1-s1",
-        artists: ["Karretta El Gucci"],
-        title: "Fake Capo",
-        remixArtists: ["Alien Cut"],
-        link: "",
-      },
-      {
-        id: "oct20-g1-s2",
-        artists: ["Carla"],
-        title: "Bim Bam Toi",
-        remixArtists: ["Alien Cut"],
-        link: "",
-      },
-      {
-        id: "oct20-g1-s3",
-        artists: ["Madison Beer"],
-        title: "Good in Goodbye",
-        remixArtists: ["Alien Cut"],
-        link: "",
-      },
-    ],
-    paypalCode: `EHN8G9PQQRGU8`,
-  },
-  november20: {
-    id: "nov20-1",
-    date: {
-      month: "novembre",
-      year: 2020,
-    },
-    songs: [
-      {
-        id: "nov20-g1-s1",
-        artists: ["Purple Disco Machine", "Sophie and the Giants"],
-        title: "Hypnotized",
-        remixArtists: ["Alien Cut"],
-        link: "",
-      },
-      {
-        id: "nov20-g1-s2",
-        artists: ["Ilona"],
-        title: "Un monde parfait",
-        remixArtists: ["Alien Cut"],
-        link: "",
-      },
-      {
-        id: "nov20-g1-s3",
-        artists: ["Cascada"],
-        title: "Night Nurse",
-        remixArtists: ["Alien Cut"],
-        link: "",
-      },
-    ],
-    paypalCode: `B5WU5F38KSGMJ`,
-  },
-  december20: {
-    id: "dic20-1",
-    date: {
-      month: "dicembre",
-      year: 2020,
-    },
-    songs: [
-      {
-        id: "dic20-g1-s1",
-        artists: ["Mabel"],
-        title: "Bum Bum",
-        remixArtists: ["Alien Cut", "Vixen"],
-        link: "",
-      },
-      {
-        id: "dic20-g1-s2",
-        artists: ["DJane HouseKat", "Rameez"],
-        title: "Ass Up",
-        remixArtists: ["Alien Cut"],
-        link: "",
-      },
-      {
-        id: "dic20-g1-s3",
-        artists: ["Alien Cut", "Mannaggia"],
-        title: "Ci siamo rotti il cazzo",
-        remixArtists: [],
-        link: "",
-      },
-    ],
-    paypalCode: `4BBS27AF6RRJC`,
-  },
-  january21: {
-    id: "gen21-1",
-    date: {
-      month: "gennaio",
-      year: 2021,
-    },
-    songs: [
-      {
-        id: "gen21-g1-s1",
-        artists: ["Piero Pelú"],
-        title: "Toro loco",
-        remixArtists: ["Alien Cut", "Miky Le Cause"],
-        link: "",
-      },
-      {
-        id: "gen21-g1-s2",
-        artists: ["Pleasurekraft"],
-        title: "Carny",
-        remixArtists: ["Alien Cut"],
-        link: "",
-      },
-      {
-        id: "gen21-g1-s3",
-        artists: ["Comaneci"],
-        title: "Satisfied Girl",
-        remixArtists: ["Alien Cut"],
-        link: "",
-      },
-    ],
-    paypalCode: `R4BXRNSF9L2E4`,
-  },
+	october20: {
+		id: "oct20-1",
+		date: {
+			month: "ottobre",
+			year: 2020,
+		},
+		songs: [
+			{
+				id: "oct20-g1-s1",
+				artists: ["Karretta El Gucci"],
+				title: "Fake Capo",
+				remixArtists: ["Alien Cut"],
+				link: "",
+			},
+			{
+				id: "oct20-g1-s2",
+				artists: ["Carla"],
+				title: "Bim Bam Toi",
+				remixArtists: ["Alien Cut"],
+				link: "",
+			},
+			{
+				id: "oct20-g1-s3",
+				artists: ["Madison Beer"],
+				title: "Good in Goodbye",
+				remixArtists: ["Alien Cut"],
+				link: "",
+			},
+		],
+		paypalCode: `EHN8G9PQQRGU8`,
+	},
+	november20: {
+		id: "nov20-1",
+		date: {
+			month: "novembre",
+			year: 2020,
+		},
+		songs: [
+			{
+				id: "nov20-g1-s1",
+				artists: ["Purple Disco Machine", "Sophie and the Giants"],
+				title: "Hypnotized",
+				remixArtists: ["Alien Cut"],
+				link: "",
+			},
+			{
+				id: "nov20-g1-s2",
+				artists: ["Ilona"],
+				title: "Un monde parfait",
+				remixArtists: ["Alien Cut"],
+				link: "",
+			},
+			{
+				id: "nov20-g1-s3",
+				artists: ["Cascada"],
+				title: "Night Nurse",
+				remixArtists: ["Alien Cut"],
+				link: "",
+			},
+		],
+		paypalCode: `B5WU5F38KSGMJ`,
+	},
+	december20: {
+		id: "dic20-1",
+		date: {
+			month: "dicembre",
+			year: 2020,
+		},
+		songs: [
+			{
+				id: "dic20-g1-s1",
+				artists: ["Mabel"],
+				title: "Bum Bum",
+				remixArtists: ["Alien Cut", "Vixen"],
+				link: "",
+			},
+			{
+				id: "dic20-g1-s2",
+				artists: ["DJane HouseKat", "Rameez"],
+				title: "Ass Up",
+				remixArtists: ["Alien Cut"],
+				link: "",
+			},
+			{
+				id: "dic20-g1-s3",
+				artists: ["Alien Cut", "Mannaggia"],
+				title: "Ci siamo rotti il cazzo",
+				remixArtists: [],
+				link: "",
+			},
+		],
+		paypalCode: `4BBS27AF6RRJC`,
+	},
+	january21: {
+		id: "gen21-1",
+		date: {
+			month: "gennaio",
+			year: 2021,
+		},
+		songs: [
+			{
+				id: "gen21-g1-s1",
+				artists: ["Piero Pelú"],
+				title: "Toro loco",
+				remixArtists: ["Alien Cut", "Miky Le Cause"],
+				link: "",
+			},
+			{
+				id: "gen21-g1-s2",
+				artists: ["Pleasurekraft"],
+				title: "Carny",
+				remixArtists: ["Alien Cut"],
+				link: "",
+			},
+			{
+				id: "gen21-g1-s3",
+				artists: ["Comaneci"],
+				title: "Satisfied Girl",
+				remixArtists: ["Alien Cut"],
+				link: "",
+			},
+		],
+		paypalCode: `R4BXRNSF9L2E4`,
+	},
 
-  february21: {
-    id: "feb21-1",
-    date: {
-      month: "febbraio",
-      year: 2021,
-    },
-    songs: [
-      {
-        id: "feb21-g1-s1",
-        artists: ["Worakls"],
-        title: "Porto",
-        remixArtists: ["Alien Cut"],
-        link: "",
-      },
-      {
-        id: "feb21-g1-s2",
-        artists: ["Depeche Mode"],
-        title: "Personal Jesus",
-        remixArtists: ["Alien Cut"],
-        link: "",
-      },
-      {
-        id: "feb21-g1-s3",
-        artists: ["Dj Satomi"],
-        title: "Castle in the Sky",
-        remixArtists: ["Alien Cut"],
-        link: "",
-      },
-    ],
-    paypalCode: `7HUTEPGVACU58`,
-  },
-  march21: {
-    id: "mar21-1",
-    date: {
-      month: "marzo",
-      year: 2021,
-    },
-    songs: [
-      {
-        id: "mar21-g1-s1",
-        artists: ["Issam Alnajjar"],
-        title: "Hadal Ahbek",
-        remixArtists: ["Alien Cut"],
-        link: "",
-      },
-      {
-        id: "mar21-g1-s2",
-        artists: ["Alien Cut"],
-        title: "Pink Panther",
-        remixArtists: [],
-        link: "",
-      },
-      {
-        id: "mar21-g1-s3",
-        artists: ["Gigi D'Agostino", "Vize", "Emotik"],
-        title: "Never be lonely",
-        remixArtists: ["Alien Cut"],
-        link: "",
-      },
-    ],
-    paypalCode: `ESZZVRVG69MXN`,
-  },
-  april21: {
-    id: "apr21-1",
-    date: {
-      month: "aprile",
-      year: 2021,
-    },
-    songs: [
-      {
-        id: "apr21-g1-s1",
-        artists: ["Mace", "Ernia", "Samurai Jay"],
-        title: "Sirena",
-        remixArtists: ["Alien Cut"],
-        link: "",
-      },
-      {
-        id: "apr21-g1-s2",
-        artists: ["Loredana Bertè"],
-        title: "Figlia di...",
-        remixArtists: ["Alien Cut"],
-        link: "",
-      },
-      {
-        id: "apr21-g1-s3",
-        artists: ["Colapesce", "Dimartino"],
-        title: "Musica leggerissima",
-        remixArtists: ["Alien Cut"],
-        link: "",
-      },
-    ],
-    paypalCode: `V4P5529NP8CMU`,
-  },
-  may21: {
-    id: "may-1",
-    date: {
-      month: "maggio",
-      year: 2021,
-    },
-    songs: [
-      {
-        id: "may21-g1-s1",
-        artists: ["Ava Max "],
-        title: "My Head & My Heart",
-        remixArtists: ["Alien Cut"],
-        link: "",
-      },
-      {
-        id: "may21-g1-s2",
-        artists: ["Alvaro Soler"],
-        title: "Magia",
-        remixArtists: ["Alien Cut"],
-        link: "",
-      },
-      {
-        id: "may21-g1-s3",
-        artists: ["Alien Cut", "Marta"],
-        title: "Isacco",
-        remixArtists: [],
-        link: "",
-      },
-    ],
-    paypalCode: `ETVK88PP7MXE8`,
-  },
+	february21: {
+		id: "feb21-1",
+		date: {
+			month: "febbraio",
+			year: 2021,
+		},
+		songs: [
+			{
+				id: "feb21-g1-s1",
+				artists: ["Worakls"],
+				title: "Porto",
+				remixArtists: ["Alien Cut"],
+				link: "",
+			},
+			{
+				id: "feb21-g1-s2",
+				artists: ["Depeche Mode"],
+				title: "Personal Jesus",
+				remixArtists: ["Alien Cut"],
+				link: "",
+			},
+			{
+				id: "feb21-g1-s3",
+				artists: ["Dj Satomi"],
+				title: "Castle in the Sky",
+				remixArtists: ["Alien Cut"],
+				link: "",
+			},
+		],
+		paypalCode: `7HUTEPGVACU58`,
+	},
+	march21: {
+		id: "mar21-1",
+		date: {
+			month: "marzo",
+			year: 2021,
+		},
+		songs: [
+			{
+				id: "mar21-g1-s1",
+				artists: ["Issam Alnajjar"],
+				title: "Hadal Ahbek",
+				remixArtists: ["Alien Cut"],
+				link: "",
+			},
+			{
+				id: "mar21-g1-s2",
+				artists: ["Alien Cut"],
+				title: "Pink Panther",
+				remixArtists: [],
+				link: "",
+			},
+			{
+				id: "mar21-g1-s3",
+				artists: ["Gigi D'Agostino", "Vize", "Emotik"],
+				title: "Never be lonely",
+				remixArtists: ["Alien Cut"],
+				link: "",
+			},
+		],
+		paypalCode: `ESZZVRVG69MXN`,
+	},
+	april21: {
+		id: "apr21-1",
+		date: {
+			month: "aprile",
+			year: 2021,
+		},
+		songs: [
+			{
+				id: "apr21-g1-s1",
+				artists: ["Mace", "Ernia", "Samurai Jay"],
+				title: "Sirena",
+				remixArtists: ["Alien Cut"],
+				link: "",
+			},
+			{
+				id: "apr21-g1-s2",
+				artists: ["Loredana Bertè"],
+				title: "Figlia di...",
+				remixArtists: ["Alien Cut"],
+				link: "",
+			},
+			{
+				id: "apr21-g1-s3",
+				artists: ["Colapesce", "Dimartino"],
+				title: "Musica leggerissima",
+				remixArtists: ["Alien Cut"],
+				link: "",
+			},
+		],
+		paypalCode: `V4P5529NP8CMU`,
+	},
+	may21: {
+		id: "may-1",
+		date: {
+			month: "maggio",
+			year: 2021,
+		},
+		songs: [
+			{
+				id: "may21-g1-s1",
+				artists: ["Ava Max "],
+				title: "My Head & My Heart",
+				remixArtists: ["Alien Cut"],
+				link: "",
+			},
+			{
+				id: "may21-g1-s2",
+				artists: ["Alvaro Soler"],
+				title: "Magia",
+				remixArtists: ["Alien Cut"],
+				link: "",
+			},
+			{
+				id: "may21-g1-s3",
+				artists: ["Alien Cut", "Marta"],
+				title: "Isacco",
+				remixArtists: [],
+				link: "",
+			},
+		],
+		paypalCode: `ETVK88PP7MXE8`,
+	},
+	june21: {
+		id: "june-1",
+		date: {
+			month: "june",
+			year: 2021,
+		},
+		songs: [
+			{
+				id: "june21-g1-s1",
+				artists: ["Marnik"],
+				title: "Up & Down",
+				remixArtists: ["Alien Cut"],
+				link: "",
+			},
+			{
+				id: "june21-g1-s2",
+				artists: ["Little Sis Nora"],
+				title: "MDMA",
+				remixArtists: ["Alien Cut"],
+				link: "",
+			},
+			{
+				id: "june21-g1-s3",
+				artists: ["Sangiovanni"],
+				title: "Malibu",
+				remixArtists: ["Alien Cut"],
+				link: "",
+			},
+		],
+		paypalCode: `UHE8VERWM7U7L`,
+	},
 };
 const parent = document.querySelector("#alien-tracklist");
 
 function tracksPackageCardTemplate(trackInfo) {
-  const month = trackInfo.date.month;
-  const year = trackInfo.date.year;
-  const songs = trackInfo.songs;
-  const paypalCode = trackInfo.paypalCode;
-  let tracksListHtml = "";
-  songs.forEach((song) => {
-    const { title, artists, remixArtists } = song;
-    tracksListHtml += `<div class="track-container">
+	const month = trackInfo.date.month;
+	const year = trackInfo.date.year;
+	const songs = trackInfo.songs;
+	const paypalCode = trackInfo.paypalCode;
+	let tracksListHtml = "";
+	songs.forEach((song) => {
+		const { title, artists, remixArtists } = song;
+		tracksListHtml += `<div class="track-container">
       <span class="small-text track">${artists.join(", ")} - ${title} ${
-      remixArtists.length ? `(${remixArtists.join(", ")} Rmx)` : ""
-    }</span>
+			remixArtists.length ? `(${remixArtists.join(", ")} Rmx)` : ""
+		}</span>
       </div>`;
-  });
+	});
 
-  let trackNodeElement = `
+	let trackNodeElement = `
     <div class="vertical-card">
     <div class="card-column main-detail">
     <p class="big-text">${month}</p>
@@ -284,9 +315,9 @@ function tracksPackageCardTemplate(trackInfo) {
     </form>
     </div>`;
 
-  return trackNodeElement;
+	return trackNodeElement;
 }
 
 Object.keys(remixesData).forEach((remix) => {
-  parent.innerHTML += tracksPackageCardTemplate(remixesData[remix]);
+	parent.innerHTML += tracksPackageCardTemplate(remixesData[remix]);
 });
