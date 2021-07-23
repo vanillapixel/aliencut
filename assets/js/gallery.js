6f5f2a98911cc4bd94df034a6cb4a88e60213f6d
vanillapixel <giorgio.adamo.ap@gmail.com>
2021-07-23 12:11:27 +0200

    new album update

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,107 +1,110 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const zoomModal = document.querySelector(".zoom-modal");
-  const carousel = document.querySelector(".carousel");
-  const zoomedPicture = document.querySelector(".carousel img");
-  const galleryPics = document.querySelectorAll(
-    ".gallery-pictures .picture img"
-  );
-  const controllers = document.querySelectorAll(".zoom-modal .controller");
-  const closeButton = document.querySelector(".close-button");
-  const backdrop = document.querySelector(".backdrop");
-  let currentPicId;
-  let transitionInProgress = false;
+	const zoomModal = document.querySelector(".zoom-modal");
+	const carousel = document.querySelector(".carousel");
+	const zoomedPicture = document.querySelector(".carousel img");
+	const galleryPics = document.querySelectorAll(
+		".gallery-pictures .picture img"
+	);
+	const controllers = document.querySelectorAll(".zoom-modal .controller");
+	const closeButton = document.querySelector(".zoom-modal .close-button");
+	const backdrop = document.querySelector(".zoom-modal .backdrop");
+	let currentPicId;
+	let transitionInProgress = false;
 
-  function closeModal() {
-    if (!transitionInProgress) {
-      deletePicture();
-      zoomModal.style.opacity = 0;
-      setTimeout(() => {
-        zoomModal.style.display = "none";
-      }, 1200);
-    }
-  }
+	function openModal() {
+		zoomModal.style.display = "flex";
+		setTimeout(() => (zoomModal.style.opacity = 1), 100);
+	}
+	function closeModal() {
+		if (!transitionInProgress) {
+			deletePicture();
+			zoomModal.style.opacity = 0;
+			setTimeout(() => {
+				zoomModal.style.display = "none";
+			}, 1200);
+		}
+	}
 
-  function createNewPicture(e, buttonPressed) {
-    const newImg = document.createElement("img");
-    currentPicId = typeof e === "object" ? e.currentTarget.id : e;
-    currentPicId;
-    newImg.src =
-      typeof e === "object" ? e.currentTarget.src : galleryPics[e].src;
-    carousel.appendChild(newImg);
-    if (buttonPressed === "next") {
-      newImg.classList.add("from-right");
-    } else {
-      newImg.classList.add("from-left");
-    }
-    setTimeout(() => {
-      newImg.classList.add("slide-in");
-    }, 200);
-  }
+	function createNewPicture(e, buttonPressed) {
+		const newImg = document.createElement("img");
+		currentPicId = typeof e === "object" ? e.currentTarget.id : e;
+		currentPicId;
+		newImg.src =
+			typeof e === "object" ? e.currentTarget.src : galleryPics[e].src;
+		carousel.appendChild(newImg);
+		if (buttonPressed === "next") {
+			newImg.classList.add("from-right");
+		} else {
+			newImg.classList.add("from-left");
+		}
+		setTimeout(() => {
+			newImg.classList.add("slide-in");
+		}, 200);
+	}
 
-  function deletePicture(buttonPressed = false) {
-    const currentPicture = document.querySelectorAll(".carousel img")[0];
-    if (buttonPressed) {
-      if (buttonPressed === "next") {
-        currentPicture.classList.add("slide-out-from-right");
-      } else {
-        currentPicture.classList.add("slide-out-from-left");
-      }
-    }
-    if (currentPicture != undefined) {
-      currentPicture.classList.add("slide-out-from-right");
-      setTimeout(() => {
-        carousel.removeChild(currentPicture);
-        transitionInProgress = false;
-      }, 1500);
-    }
-  }
+	function deletePicture(buttonPressed = false) {
+		const currentPicture = document.querySelectorAll(".carousel img")[0];
+		if (buttonPressed) {
+			if (buttonPressed === "next") {
+				currentPicture.classList.add("slide-out-from-right");
+			} else {
+				currentPicture.classList.add("slide-out-from-left");
+			}
+		}
+		if (currentPicture != undefined) {
+			currentPicture.classList.add("slide-out-from-right");
+			setTimeout(() => {
+				carousel.removeChild(currentPicture);
+				transitionInProgress = false;
+			}, 1500);
+		}
+	}
 
-  function updateDisplayedPicture(buttonPressed) {
-    // if next is pressed, id increases
-    if (buttonPressed === "next") {
-      deletePicture(buttonPressed);
-      //if it's the last picture it starts back from the beginning
-      currentPicId == galleryPics.length - 1
-        ? (currentPicId = 0)
-        : currentPicId++;
-      createNewPicture(currentPicId, buttonPressed);
-    } else {
-      deletePicture(buttonPressed);
-      // if it's the first picture it starts back from the end
-      currentPicId == 0
-        ? (currentPicId = galleryPics.length - 1)
-        : currentPicId--;
-      createNewPicture(currentPicId, buttonPressed);
-    }
-  }
+	function updateDisplayedPicture(buttonPressed) {
+		// if next is pressed, id increases
+		if (buttonPressed === "next") {
+			deletePicture(buttonPressed);
+			//if it's the last picture it starts back from the beginning
+			currentPicId == galleryPics.length - 1
+				? (currentPicId = 0)
+				: currentPicId++;
+			createNewPicture(currentPicId, buttonPressed);
+		} else {
+			deletePicture(buttonPressed);
+			// if it's the first picture it starts back from the end
+			currentPicId == 0
+				? (currentPicId = galleryPics.length - 1)
+				: currentPicId--;
+			createNewPicture(currentPicId, buttonPressed);
+		}
+	}
 
-  galleryPics.forEach((pic, id) => {
-    pic.id = id;
-    pic.addEventListener("click", (e) => {
-      if (!isMobile()) {
-        deletePicture();
-        createNewPicture(e);
-        zoomModal.style.display = "flex";
-        setTimeout(() => (zoomModal.style.opacity = 1), 100);
-      }
-    });
-  });
+	galleryPics.forEach((pic, id) => {
+		pic.id = id;
+		pic.addEventListener("click", (e) => {
+			if (!isMobile()) {
+				deletePicture();
+				createNewPicture(e);
+				openModal();
+			}
+		});
+	});
 
-  controllers.forEach((controller) => {
-    controller.addEventListener("click", function () {
-      if (!transitionInProgress) {
-        transitionInProgress = true;
-        const buttonClasses = this.classList.value.split(" ");
-        const buttonClass = buttonClasses
-          .filter((x) => x != "controller")
-          .toString();
-        updateDisplayedPicture(buttonClass);
-      }
-    });
-  });
+	controllers.forEach((controller) => {
+		controller.addEventListener("click", function () {
+			if (!transitionInProgress) {
+				transitionInProgress = true;
+				const buttonClasses = this.classList.value.split(" ");
+				const buttonClass = buttonClasses
+					.filter((x) => x != "controller")
+					.toString();
+				updateDisplayedPicture(buttonClass);
+			}
+		});
+	});
 
-  closeButton.addEventListener("click", closeModal);
-  backdrop.addEventListener("click", closeModal);
+	closeButton.addEventListener("click", closeModal);
+	backdrop.addEventListener("click", closeModal);
 });
 
 // TODO: Pics display animation to finish
@@ -109,35 +112,35 @@ document.addEventListener("DOMContentLoaded", () => {
 const pics = document.querySelectorAll(".picture");
 
 function picsIn() {
-  pics.forEach((x, id) => {
-    x.style.opacity = 1;
-    x.style.transform = "translateY(0px)";
-    x.style.transition = `1s ${id * 0.1}s`;
-  });
+	pics.forEach((x, id) => {
+		x.style.opacity = 1;
+		x.style.transform = "translateY(0px)";
+		x.style.transition = `1s ${id * 0.1}s`;
+	});
 }
 
 function picsOut() {
-  pics.forEach((x, id) => {
-    const position = x.getBoundingClientRect().top;
-    x.style.opacity = 0;
-    x.style.transition = `1s ${id * 0.1}s`;
-    let mode;
-    const COEFFICIENT = 4;
-    const translation = {
-      1: `-${COEFFICIENT * 100}px`,
-      2: "0px",
-      3: `${COEFFICIENT * 100}px`,
-    };
-    if (position > 200 && position < 398) {
-      mode = 2;
-    }
+	pics.forEach((x, id) => {
+		const position = x.getBoundingClientRect().top;
+		x.style.opacity = 0;
+		x.style.transition = `1s ${id * 0.1}s`;
+		let mode;
+		const COEFFICIENT = 4;
+		const translation = {
+			1: `-${COEFFICIENT * 100}px`,
+			2: "0px",
+			3: `${COEFFICIENT * 100}px`,
+		};
+		if (position > 200 && position < 398) {
+			mode = 2;
+		}
 
-    if (position < 199) {
-      mode = 1;
-    }
-    if (position > 399) {
-      mode = 3;
-    }
-    x.style.transform = `translateY(${translation[mode]}) scale(2)`;
-  });
+		if (position < 199) {
+			mode = 1;
+		}
+		if (position > 399) {
+			mode = 3;
+		}
+		x.style.transform = `translateY(${translation[mode]}) scale(2)`;
+	});
 }
--- assets/js/misc.js
+++ assets/js/misc.js
@@ -1,38 +1,92 @@
-function initPayPalButton() {
-  paypal
-    .Buttons({
-      style: {
-        shape: "pill",
-        color: "gold",
-        layout: "horizontal",
-        label: "buynow",
-        tagline: true,
-      },
+const codeSubmit = document.querySelector(".icon-container svg");
+const specialTracksInput = document.querySelector("#special-tracks-code");
+const codeForm = document.querySelector("#code-form");
+const body = document.querySelector("body");
+const codeModal = document.querySelector(".code-modal");
+const closeButton = document.querySelector(".code-modal .close-button");
+const backdrop = document.querySelector(".code-modal .backdrop");
 
-      createOrder: function (data, actions) {
-        return actions.order.create({
-          purchase_units: [
-            {
-              description: "Bracciale Alien nero/oro",
-              amount: { currency_code: "EUR", value: 1 },
-            },
-          ],
-        });
-      },
+const codeModalMessage = document.querySelector(".code-modal .vertical-card");
+const codeModalWrapper = document.createElement("div");
 
-      onApprove: function (data, actions) {
-        return actions.order.capture().then(function (details) {
-          alert(
-            "Transaction completed by " + details.payer.name.given_name + "!"
-          );
-          console.log(data);
-        });
-      },
+const trackDownloadBtn = `
+	<a
+		class="cta-button pulse"
+		href="./assets/uploads/extended_tracks_mcsv2.rar"
+		rel="noreferrer"
+		target="_blank"
+	>
+		Scarica le tracce
+	</a>`;
 
-      onError: function (err) {
-        console.log(err);
-      },
-    })
-    .render("#paypal-button-container");
+// const codeSubmit = document.querySelector("#download-btn");
+
+function openModal() {
+	codeModal.style.display = "flex";
+	setTimeout(() => (codeModal.style.opacity = 1), 100);
 }
-// setTimeout(initPayPalButton, 2500);
+function closeModal() {
+	codeModal.style.opacity = 0;
+	setTimeout(() => {
+		body.reoveChild(codeModal);
+	}, 1200);
+}
+
+function codeChecker(e) {
+	e.preventDefault();
+	// error management
+	// code is wrong,
+	// code is empty
+
+	if (specialTracksInput.value === "MCS2ACEXT5") {
+		// open modal
+		openModal();
+		codeModalMessage.innerHTML += trackDownloadBtn;
+		// link tracce
+	} else {
+		alert("Il codice inserito non è valido");
+	}
+}
+codeSubmit.addEventListener("click", codeChecker);
+codeForm.addEventListener("submit", codeChecker);
+closeButton.addEventListener("click", closeModal);
+backdrop.addEventListener("click", closeModal);
+
+// function initPayPalButton() {
+//   paypal
+//     .Buttons({
+//       style: {
+//         shape: "pill",
+//         color: "gold",
+//         layout: "horizontal",
+//         label: "buynow",
+//         tagline: true,
+//       },
+
+//       createOrder: function (data, actions) {
+//         return actions.order.create({
+//           purchase_units: [
+//             {
+//               description: "Bracciale Alien nero/oro",
+//               amount: { currency_code: "EUR", value: 1 },
+//             },
+//           ],
+//         });
+//       },
+
+//       onApprove: function (data, actions) {
+//         return actions.order.capture().then(function (details) {
+//           alert(
+//             "Transaction completed by " + details.payer.name.given_name + "!"
+//           );
+//           console.log(data);
+//         });
+//       },
+
+//       onError: function (err) {
+//         console.log(err);
+//       },
+//     })
+//     .render("#paypal-button-container");
+// }
+// // setTimeout(initPayPalButton, 2500);
